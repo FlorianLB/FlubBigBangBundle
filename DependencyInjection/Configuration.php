8a303554c67a51be70a10c6b1addaa1d4871b825
FlorianLB <flonqueubrochard@clever-age.com>
2014-02-08 16:52:34 +0100

    remove dead code

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('flub_bigbang');
+        $treeBuilder->root('flub_bigbang');
 
         return $treeBuilder;
     }
